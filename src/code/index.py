1ee7e82321172e87e95dc003d3fdcb337a7d8f50
yusha.xy <yusha.xy@alibaba-inc.com>
2022-12-02 14:21:38 +0800

    fix

--- src/code/index.py
+++ src/code/index.py
@@ -11,6 +11,7 @@ from wand.image import Image
 from wand.drawing import Drawing
 from wand.color import Color
 
+
 def oss_get_image_data(context, path):
     parts = path.split('/')
     bucket = parts[0]
@@ -21,6 +22,7 @@ def oss_get_image_data(context, path):
     oss_client = oss2.Bucket(auth, 'oss-' + context.region +
                              '-internal.aliyuncs.com', bucket)
     return oss_client.get_object(object)
+
 
 def oss_save_image_data(context, img, path):
     parts = path.split('/')
@@ -33,6 +35,7 @@ def oss_save_image_data(context, img, path):
                              '-internal.aliyuncs.com', bucket)
     oss_client.put_object(object, img.make_blob())
 
+
 def make_response(context, img):
     target = bottle.request.query.get('target')
     if target is not None and len(target) > 0:
@@ -43,6 +46,7 @@ def make_response(context, img):
         bottle.response.content_type = 'image/jpeg'
         return img.make_blob()
 
+
 @bottle.route('/pinjie', method='GET')
 def pinjie():
     try:
@@ -52,14 +56,15 @@ def pinjie():
         context = bottle.request.environ.get('fc.context')
         with Image(file=oss_get_image_data(context, image1)) as f1:
             with Image(file=oss_get_image_data(context, image2)) as f2:
-                with Image(width=f1.width+f2.width+10, height=max(f1.height, f2.height)) as img:
+                with Image(width=f1.width + f2.width + 10, height=max(f1.height, f2.height)) as img:
                     img.format = 'jpg'
                     img.composite(f1, left=0, top=0)
-                    img.composite(f2, left=f1.width+10, top=0)
+                    img.composite(f2, left=f1.width + 10, top=0)
                     return make_response(context, img)
     except Exception as ex:
         bottle.response.content_type = 'text/plain'
         return 'ERROR: ' + traceback.format_exc()
+
 
 @bottle.route('/watermark', method='GET')
 def watermark():
@@ -73,7 +78,7 @@ def watermark():
             draw.fill_color = Color('red')
             draw.font_size = 24
             with Image(width=200, height=50, background=Color('transparent')) as pic:
-                draw.text(10, int(pic.height/2), text)
+                draw.text(10, int(pic.height / 2), text)
                 draw(pic)
                 pic.alpha = True
                 pic.format = 'png'
@@ -83,7 +88,7 @@ def watermark():
             with Image(filename='/tmp/water.png') as water:
                 with img.clone() as base:
                     base.format = 'jpg'
-                    base.watermark(water, 0.3, 0, int(img.height-30))
+                    base.watermark(water, 0.3, 0, int(img.height - 30))
                     return make_response(context, base)
     except Exception as ex:
         bottle.response.content_type = 'text/plain'
@@ -104,37 +109,43 @@ def format():
         bottle.response.content_type = 'text/plain'
         return 'ERROR: ' + traceback.format_exc()
 
-@bottle.route('/gray/<path:path>')
-def gray(path):
+
+@bottle.route('/gray', method='GET')
+def gray():
     try:
-        OSS_BUCKET_NAME = 'zc-tmp'
+        path = bottle.request.query.get('img')
+        parts = path.split('/')
+        bucket = parts[0]
+        object = '/'.join(parts[1:])
+        img_type = object.split('.')[-1]
         context = bottle.request.environ.get('fc.context')
-        path_info = path
         creds = context.credentials
         auth = oss2.StsAuth(creds.accessKeyId,
                             creds.accessKeySecret, creds.securityToken)
-        bucket = oss2.Bucket(auth, 'http://oss-cn-hangzhou.aliyuncs.com', OSS_BUCKET_NAME)
-        fpath = '/tmp/' + str(uuid.uuid4()) + '_' + os.path.split(path_info)[-1]
-        bucket.get_object_to_file(path_info, fpath)
+        oss_client = oss2.Bucket(auth, 'oss-' + context.region +
+                                 '-internal.aliyuncs.com', bucket)
+        fpath = '/tmp/' + str(uuid.uuid4()) + '_' + os.path.split(object)[-1]
+        oss_client.get_object_to_file(object, fpath)
         image = cv2.imread(fpath)
 
         # 转为灰度图像
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        wpath = fpath.replace(".", "_") + '.png'
+        wpath = fpath.replace(".", "_") + '.'+img_type
         cv2.imwrite(wpath, image)
         f = open(wpath, 'rb')
         result = f.read()
         bottle.response.status = '200 OK'
-        bottle.response.content_type = 'image/png'
-        bucket.delete_object(fpath)
+        bottle.response.content_type = 'image/'+img_type
         return result
     except Exception as ex:
         bottle.response.content_type = 'text/plain'
         return 'ERROR: ' + traceback.format_exc()
 
-@ bottle.route('/', method='GET')
+
+@bottle.route('/', method='GET')
 def index():
     return bottle.template('./index.html')
+
 
 app = bottle.default_app()
 
